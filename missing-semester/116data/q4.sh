03dfa6c917267019a737453b90a97f930123d7fd
stever178 <2874146120@qq.com>
2024-09-07 19:42:06 +0800

    little update

--- missing-semester/116data/q4.sh
+++ missing-semester/116data/q4.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
 
-cat starttime.txt | grep "systemd\[1\]" | sed -E "s/.*=\ (.*)s\.$/\1/" | sed "s/min /*60.0+/" | bc -l | sort -n
+OUT=starttime.txt
+if [ ! -f $OUT ]; then
+	bash getlog.sh 2>/dev/null > "$OUT"
+fi
+
+cat starttime.txt | grep "systemd\[1\]" | sed -E "s/.*=\ (.*)s\.$/\1/" 
+echo
+cat starttime.txt | grep "systemd\[1\]" | sed -E "s/.*=\ (.*)s\.$/\1/" | sed "s/min /*60.0+/" | bc -l | sort -n 
